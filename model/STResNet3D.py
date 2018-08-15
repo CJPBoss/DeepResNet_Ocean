5f627af87782a9444e0be6641d3ec6040121b21d
Tim Liu <1349391871@qq.com>
2018-08-15 17:01:04 +0800

    Add files via upload
    single res_unit_v1.0

--- model/STResNet3D.py
+++ model/STResNet3D.py
@@ -1,23 +1,11 @@
 import tensorflow as tf
 
-def _shortcut(input, residual):
-    return tf.add(input, residual)
-
-def _bn_relu_conv3d(inputs,
-                    filters,
-                    kernel_size,
-                    strides,
-                    bn=False,
-                    name=None):
+def ResUnit3D(inputs, filters = 64, kernel_size = 3,
+              strides = 1, bn=False, name=None):
     if name is None:
         name = ''
-    sess = tf.Session()
     with tf.name_scope("Residual_Unit_" + name) as res_unit:
-    
-        print('input\n', sess.run(inputs)[-1][-1][-1])
-        activation1 = tf.nn.relu(inputs, name='activation_1')
-        print('av1', sess.run(activation1)[-1][-1][-1])
-        print(filters, kernel_size, strides)
+        activation1 = tf.nn.relu(inputs, name='relu_1')
         conv1 = tf.layers.conv3d(
             inputs=activation1,
             filters=filters,
@@ -26,9 +14,7 @@ def _bn_relu_conv3d(inputs,
             padding='same',
             name='conv_1'
         )
-        print('conv1\n', sess.run(conv1))
-        activation2 = tf.nn.relu(conv1, name='activation_2')
-        print('av2\n', sess.run(activation2))
+        activation2 = tf.nn.relu(conv1, name='relu_2')
         conv2 = tf.layers.conv3d(
             activation2,
             filters=filters,
@@ -37,21 +23,11 @@ def _bn_relu_conv3d(inputs,
             padding='same',
             name='conv_2'
         )
-        print('conv2\n', sess.run(conv2))
         output = tf.add(inputs, conv2, name='add')
-        print('add\n', sess.run(output))
     return output
     
-'''    
-    
-    
-def ResUnits3D(input,
-               size=(None, 60, 20, 20),
-               repetations=1,
-               name=None,):
-    
-    
 
+'''
 
 def STResNet3D(
                 c_conf=(3, 60, 20, 20),
--- model/test_relu_conv.py
+++ model/test_relu_conv.py
@@ -4,11 +4,35 @@ import numpy as np
 
 if __name__ == '__main__':
     matx = np.random.normal(size=1000).reshape((-1, 10, 10, 10, 1))
-    matx = tf.constant(matx)
-    ##print(matx)
+    matx = tf.Variable(matx)
+    print(matx)
+    print('============================')
+    print('relu conv')
+    a = _bn_relu_conv3d(matx, filters=10, kernel_size=3, strides=1, bn=False, name='Test')
+    merged = tf.summary.merge_all()
+    
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    print(sess.run(matx)[-1][-1][-1])
+    print(sess.run(a)[-1][-1][-1])
+    
+    writer = tf.summary.FileWriter('test/residual_unit', sess.graph)
+    '''
     sess = tf.Session()
     print('============================')
-    #print(sess.run(matx))
-    #print('relu conv')
-    a = _bn_relu_conv3d(matx, filters=10, kernel_size=(3, 3, 3), strides=(1, 1, 1), bn=False, name='Test')
-    print(sess.run(a))
+    #matx = np.random.normal(size=1000).reshape((-1, 10, 10, 10, 1))
+    matx = np.arange(27).reshape((-1, 3, 3, 3, 1))
+    matx = tf.Variable(matx, dtype=tf.float32)
+    #sess.run(tf.initialize_all_variables())
+    c3 = tf.layers.conv3d(
+        inputs=matx,
+        filters=1,
+        kernel_size=3,
+        strides=1,
+        activation=None,
+        padding='same'
+    )
+    sess.run(tf.global_variables_initializer())
+    print(sess.run(matx)[-1][-1][-1])
+    print(sess.run(c3)[-1][-1][-1])
+    '''
