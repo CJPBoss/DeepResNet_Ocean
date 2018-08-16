0bb9357ff2e40e54d33e81a2442f80f9987af265
unknown <seermf8@163.com>
2018-08-16 11:05:38 +0800

    comment for stresnet3d.py

--- model/STResNet3D.py
+++ model/STResNet3D.py
@@ -2,39 +2,67 @@ import tensorflow as tf
 
 def _residual_unit_3d(inputs, filters=64, kernel_size=3,
                       strides=1, name=None):
+    '''
+    # a single residual unit,
+    # inputs -> relu -> conv3d -> relu -> conv3d -> add -> outputs
+    #    |                                           ^
+    #    |___________________________________________|
+    #
+    # Arguments:
+    #   inputs: a 5-D tensor, [batch, depth, height, width, channel]
+    #   filters: Integer, the dimensionality of the output space.
+    #   kernel_size: An integer or tuple/list of n integers, specifying the
+    #       length of the convolution window.
+    #   strides: An integer or tuple/list of n integers,
+    #       specifying the stride length of the convolution.
+    #       Specifying any stride value != 1 is incompatible with specifying
+    #       any `dilation_rate` value != 1.
+    #   name: A string, the name of the layer.
+    #
+    # Output:
+    #   a 5-D tensor, which shape is the same as inputs'.
+    '''
     if name is None:
         name = ''
-    print(name)
-    with tf.name_scope("Res_Unit_" + name) as res_unit:
-        activation0 = tf.nn.relu(inputs, name='relu_0') 
-        conv0 = tf.layers.conv3d(
+    with tf.name_scope("Res_Unit_" + name) as res_unit: # res unit
+        activation0 = tf.nn.relu(inputs, name='relu_0') # relu 0
+        conv0 = tf.layers.conv3d( # conv3d 0
             inputs=activation0,
             filters=filters,
             kernel_size=kernel_size,
             strides=strides,
             padding='same',
             name='res_conv_'+name+'_0'
-        )
-        activation1 = tf.nn.relu(conv0, name='relu_1')
-        conv1 = tf.layers.conv3d(
+        ) 
+        activation1 = tf.nn.relu(conv0, name='relu_1') # relu 1
+        conv1 = tf.layers.conv3d( # conv3d 1
             activation1,
             filters=filters,
             kernel_size=kernel_size,
             strides=strides,
             padding='same',
             name='res_conv_'+name+'_1'
-        )
-        output = tf.add(inputs, conv1, name='add')
+        ) 
+        output = tf.add(inputs, conv1, name='add') # add the inputs and conv3d
     return output
     
 def res_units_3d(inputs, res_unit=_residual_unit_3d,
                  filters=64, kernel_size=3, strides=1,
-                 name=None, repetations=1):
+                 name=None, num_res_units=1):
+    '''
+    # a sequence of residual units,
+    #
+    # Arguments:
+    #   num_res_units: the number of residual units
+    # 
+    # Output:
+    #   a 5-D tensor, which shape is the same as inputs'.
+    '''
     if name is None:
         name=''
-    with tf.name_scope("Res_Units_" + name) as res_units:
-        for i in range(repetations):
-            print(i)
+    with tf.name_scope("Res_Units_" + name) as res_units: # residual units
+        for i in range(num_res_units): # generate resunits which amount is
+                                       # the same as num_res_units
             inputs = res_unit(inputs, filters=filters,
                               kernel_size=kernel_size,
                               strides=strides,
@@ -48,19 +76,27 @@ def STResNet3D(input_c, input_p, input_t,
                strides=((1, 1, 1), (1, 1, 1), (1, 1, 1)),
                num_res_units=(3, 3, 3),
                name=None):
+    '''
+    # Deep Spatio-Temporal Residual Networks.
+    #
+    # Arguments:
+    #   input_c, input_p, input_t:
+    #       a 5-D tensor, the input of Property closeness, period and trend.
+    #   
+    '''
     if name is None:
-        name = ''
+        name = 'STResNet3D'
     inputs = [input_c, input_p, input_t]
-    outputs = []
-    with tf.name_scope("ST_Res_Net_3D") as stresnet3d:
-        for i in range(3):
+    outputs = [] # output list of each property
+    with tf.name_scope(name) as stresnet3d:
+        for i in range(3): # 3 properties
             id = str(i)
             input = inputs[i]
             if input is None:
                 continue
-            #DHWC = input.shape[1:]  #depth, height, width, channel
-            with tf.name_scope("Process" + str(i)) as process:
-                preconv = tf.layers.conv3d(
+            with tf.name_scope("Property" + id) as process:
+                preconv = tf.layers.conv3d( # use conv3d to make the shape of
+                                            # tensor suit the res unit
                     inputs=input,
                     filters=filters[i],
                     kernel_size=kernel_size[i],
@@ -68,23 +104,23 @@ def STResNet3D(input_c, input_p, input_t,
                     padding='same',
                     name='Pre_conv_' + id
                 )
-                resunits = res_units_3d(
+                resunits = res_units_3d( # res units
                     inputs=preconv,
                     res_unit=_residual_unit_3d,
                     filters=filters[i],
                     kernel_size=kernel_size[i],
                     strides=strides[i],
-                    name='Pro_' + id,
-                    repetations=num_res_units[i]
+                    name=id,
+                    num_res_units=num_res_units[i]
                 )
-                activation = tf.nn.relu(resunits)
-                output = tf.layers.conv3d(
+                activation = tf.nn.relu(resunits) # relu activation
+                output = tf.layers.conv3d( # res unit's output
                     inputs=activation,
                     filters=1,
                     kernel_size=kernel_size[i],
                     strides=strides[i],
                     padding='same',
-                    name='ResUnit_output_' + id
+                    name='Res_' + id + 'out'
                 )
             outputs.append(output)
         main_output = None
@@ -94,7 +130,7 @@ def STResNet3D(input_c, input_p, input_t,
         else:
             new_outputs = []
             for i in range(len(outputs)):
-                newoutput = tf.layers.conv3d(
+                newoutput = tf.layers.conv3d( # distribute weights for each output
                     outputs[i],
                     filters=1,
                     kernel_size=1,
@@ -103,13 +139,9 @@ def STResNet3D(input_c, input_p, input_t,
                     name='weights' + str(i)
                 )
                 new_outputs.append(newoutput)
-            '''
-            main_output = new_outputs[0]
-            for i in range(len(new_outputs) - 1):
-                main_output += new_outputs[i + 1]
-            '''
-            main_output = sum(new_outputs)
-    output = tf.nn.tanh(main_output, name='OUTPUT')    
+            main_output = sum(new_outputs) # merge all the outputs
+    output = tf.nn.tanh(main_output, name=name + 'Output') # employ tanh to
+                                                           # activate the output
     return output
         
         
